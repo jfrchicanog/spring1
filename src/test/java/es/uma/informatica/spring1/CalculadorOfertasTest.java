c5bb0666a9a78963c1475456a3962eabf09b3fad
francis <chicano@lcc.uma.es>
2016-12-01 00:48:53 +0100

    Factura en XML

--- src/test/java/es/uma/informatica/spring1/CalculadorOfertasTest.java
+++ src/test/java/es/uma/informatica/spring1/CalculadorOfertasTest.java
@@ -1,8 +1,5 @@
 package es.uma.informatica.spring1;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,8 +7,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import es.uma.informatica.spring1.domain.Factura;
-import es.uma.informatica.spring1.domain.LineaFactura;
-import es.uma.informatica.spring1.domain.Producto;
 
 public class CalculadorOfertasTest {
 	private ApplicationContext contexto;
@@ -24,41 +19,7 @@ public class CalculadorOfertasTest {
 	@Test
 	public void test() {
 		
-		List<LineaFactura> lineas = new ArrayList<>();
-		
-		Producto producto = new Producto();
-		producto.setNombre("Leche");
-		producto.setPrecio(1.50);
-		
-		LineaFactura linea = new LineaFactura();
-		linea.setProducto(producto);
-		linea.setCantidad(6);
-		
-		lineas.add(linea);
-		
-		producto = new Producto();
-		producto.setNombre("Queso");
-		producto.setPrecio(3.40);
-		
-		linea = new LineaFactura();
-		linea.setProducto(producto);
-		linea.setCantidad(2);
-		
-		lineas.add(linea);
-		
-		producto = new Producto();
-		producto.setNombre("Atún");
-		producto.setPrecio(3.00);
-		
-		linea = new LineaFactura();
-		linea.setProducto(producto);
-		linea.setCantidad(6);
-		
-		lineas.add(linea);
-		
-		Factura factura = new Factura();
-		factura.setLineas(lineas);
-		
+		Factura factura = contexto.getBean(Factura.class);
 		CalculadorOfertas calculador = contexto.getBean(CalculadorOfertas.class);
 		
 		calculador.calcularOfertas(factura);
