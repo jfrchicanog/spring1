78c023b23757e64097b0818b9e52e48323e290d4
francis <chicano@lcc.uma.es>
2016-12-01 01:25:02 +0100

    Calculadora con anotaciones

--- src/main/java/es/uma/informatica/spring1/CalculadorOfertas.java
+++ src/main/java/es/uma/informatica/spring1/CalculadorOfertas.java
@@ -3,16 +3,23 @@ package es.uma.informatica.spring1;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
 import es.uma.informatica.spring1.domain.Factura;
 import es.uma.informatica.spring1.domain.LineaFactura;
 
+@Component
 public class CalculadorOfertas {
 	private List<Oferta> ofertas;
 	
-	public CalculadorOfertas(List<Oferta> ofertas) {
+	public List<Oferta> getOfertas() {
+		return ofertas;
+	}
+
+	public void setOfertas(List<Oferta> ofertas) {
 		this.ofertas = ofertas;
 	}
-	
+
 	public void calcularOfertas(Factura factura) {
 		List<LineaFactura> lineasOfertas = new ArrayList<>();
 		for (Oferta oferta: ofertas) {
