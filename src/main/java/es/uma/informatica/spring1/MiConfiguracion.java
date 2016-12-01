b4e66ff643393b4ec5245bb28d699b5f4ab171ba
francis <chicano@lcc.uma.es>
2016-12-01 01:36:34 +0100

    cOnfiguración con java bean

---
+++ src/main/java/es/uma/informatica/spring1/MiConfiguracion.java
@@ -0,0 +1,66 @@
+package es.uma.informatica.spring1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import es.uma.informatica.spring1.domain.Factura;
+import es.uma.informatica.spring1.domain.LineaFactura;
+import es.uma.informatica.spring1.domain.Producto;
+
+@Configuration
+public class MiConfiguracion {
+	@Bean
+	public CalculadorOfertas getCalculadorOfertas() {
+
+		List<Oferta> ofertas = new ArrayList<>();
+		ofertas.add(new Oferta3x2("Leche"));
+		ofertas.add(new Descuento20Porciento("Atún"));
+		
+		CalculadorOfertas calculador = new CalculadorOfertas(ofertas);
+		return calculador;
+	}
+	
+	@Bean
+	public Factura getFactura() {
+		List<LineaFactura> lineas = new ArrayList<>();
+		
+		Producto producto = new Producto();
+		producto.setNombre("Leche");
+		producto.setPrecio(1.50);
+		
+		LineaFactura linea = new LineaFactura();
+		linea.setProducto(producto);
+		linea.setCantidad(6);
+		
+		lineas.add(linea);
+		
+		producto = new Producto();
+		producto.setNombre("Queso");
+		producto.setPrecio(3.40);
+		
+		linea = new LineaFactura();
+		linea.setProducto(producto);
+		linea.setCantidad(2);
+		
+		lineas.add(linea);
+		
+		producto = new Producto();
+		producto.setNombre("Atún");
+		producto.setPrecio(3.00);
+		
+		linea = new LineaFactura();
+		linea.setProducto(producto);
+		linea.setCantidad(6);
+		
+		lineas.add(linea);
+		
+		Factura factura = new Factura();
+		factura.setLineas(lineas);
+		
+		return factura;
+	}
+
+}
--- src/test/java/es/uma/informatica/spring1/CalculadorOfertasTest.java
+++ src/test/java/es/uma/informatica/spring1/CalculadorOfertasTest.java
@@ -1,61 +1,26 @@
 package es.uma.informatica.spring1;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import es.uma.informatica.spring1.domain.Factura;
-import es.uma.informatica.spring1.domain.LineaFactura;
-import es.uma.informatica.spring1.domain.Producto;
 
 public class CalculadorOfertasTest {
+	private ApplicationContext context;
+	
+	@Before
+	public void setup() {
+		context = new AnnotationConfigApplicationContext(MiConfiguracion.class);
+	}
 
 	@Test
 	public void test() {
-		
-		List<LineaFactura> lineas = new ArrayList<>();
-		
-		Producto producto = new Producto();
-		producto.setNombre("Leche");
-		producto.setPrecio(1.50);
-		
-		LineaFactura linea = new LineaFactura();
-		linea.setProducto(producto);
-		linea.setCantidad(6);
-		
-		lineas.add(linea);
-		
-		producto = new Producto();
-		producto.setNombre("Queso");
-		producto.setPrecio(3.40);
-		
-		linea = new LineaFactura();
-		linea.setProducto(producto);
-		linea.setCantidad(2);
-		
-		lineas.add(linea);
-		
-		producto = new Producto();
-		producto.setNombre("Atún");
-		producto.setPrecio(3.00);
-		
-		linea = new LineaFactura();
-		linea.setProducto(producto);
-		linea.setCantidad(6);
-		
-		lineas.add(linea);
-		
-		Factura factura = new Factura();
-		factura.setLineas(lineas);
-		
-		List<Oferta> ofertas = new ArrayList<>();
-		ofertas.add(new Oferta3x2("Leche"));
-		ofertas.add(new Descuento20Porciento("Atún"));
-		
-		CalculadorOfertas calculador = new CalculadorOfertas(ofertas);
-		
+
+		CalculadorOfertas calculador = context.getBean(CalculadorOfertas.class);
+		Factura factura = context.getBean(Factura.class);
 		calculador.calcularOfertas(factura);
 		
 		Assert.assertEquals(27.20, factura.getTotal(), 0.001);
@@ -64,5 +29,6 @@ public class CalculadorOfertasTest {
 		System.out.println(factura.toString());
 		
 	}
+
 
 }
